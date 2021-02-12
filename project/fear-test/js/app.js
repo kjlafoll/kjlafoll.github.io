5e4a4d95b7d6aca180c61b33f52072331aa4acde
kjlafoll <32970290+kjlafoll@users.noreply.github.com>
2021-02-12 16:42:23 -0500

    Update app.js

--- project/fear-test/js/app.js
+++ project/fear-test/js/app.js
@@ -42,10 +42,10 @@ function runCS() {
 	trialtext = JSON.stringify(mysetup[listc-1])
 	if (mysetup[listc-1]["cs_type"] == "CS-") {
 		srcfile = '<image src=' + '"Resources_CONDexp/CS_IMAGES/CS_NEG.png"' +
-			' style="width:25%;height:0"' + '>';
+			' style="width:25%"' + '>';
 	} else if (mysetup[listc-1]["cs_type"] == "CS+") {
 		srcfile = '<image src=' + '"Resources_CONDexp/CS_IMAGES/CS_POS.png"' +
-			' style="width:25%;height:0"' + '>';
+			' style="width:25%"' + '>';
 	}
 	prestext.innerHTML = "<body>" +
 		trialtext +
