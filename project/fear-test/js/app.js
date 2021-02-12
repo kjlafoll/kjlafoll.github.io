191920c3a80604632cb9485cd2c85397d921c43e
kjlafoll <32970290+kjlafoll@users.noreply.github.com>
2021-02-12 16:31:49 -0500

    update

--- project/fear-test/js/app.js
+++ project/fear-test/js/app.js
@@ -41,16 +41,16 @@ function runFixation() {
 function runCS() {
 	trialtext = JSON.stringify(mysetup[listc-1])
 	if (mysetup[listc-1]["cs_type"] == "CS-") {
-		srcfile = '<image src=' + '"Resources_CONDexp/CS_IMAGES/CS_NEG.png"' + '>';
+		srcfile = '<image src=' + '"Resources_CONDexp/CS_IMAGES/CS_NEG.png"' +
+			' style="width:30px;height:px"' + '>';
 	} else if (mysetup[listc-1]["cs_type"] == "CS+") {
-		srcfile = '<image src=' + '"Resources_CONDexp/CS_IMAGES/CS_POS.png"' + '>';
+		srcfile = '<image src=' + '"Resources_CONDexp/CS_IMAGES/CS_POS.png"' +
+			' style="width:30px;height:px"' + '>';
 	}
 	prestext.innerHTML = "<body>" +
 		trialtext +
 		"</body>" +
-		'<div id="controls">' +
-		srcfile +
-		'</div>';
+		srcfile;
 }
 
 function stopAction() {
