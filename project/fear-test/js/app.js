22e05a28b5e2e589a7b51a980693e80930f69ea4
kjlafoll <32970290+kjlafoll@users.noreply.github.com>
2021-02-13 09:29:32 -0500

    Update app.js

--- project/fear-test/js/app.js
+++ project/fear-test/js/app.js
@@ -158,7 +158,7 @@ function savedata() {
 				console.log(data);
 			},
 			error: function(xhr, textStatus, errorThrown){
-				console.log(errorThrown);
+				console.log(textStatus);
 			}
 		})
 	});
