2e709525203ba3e4646305aa0b035e0863bd70c2
kjlafoll <32970290+kjlafoll@users.noreply.github.com>
2021-02-13 09:52:33 -0500

    Update app.js

--- project/fear-test/js/app.js
+++ project/fear-test/js/app.js
@@ -153,6 +153,7 @@ function savedata() {
 				'Authorization': "bearer " + apiKey,
 				'Access-Control-Allow-Origin': '*',
 				'Access-Control-Allow-Methods': 'GET, POST, PUT',
+				'Access-Control-Allow-Headers': "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
 			},
 			contentType: 'application/json',
 			dataType: 'json',
