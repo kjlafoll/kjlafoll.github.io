5eeb1916cc91e4d17effd9b9ee5748ffb619ff16
Alessandra Puccio <agp63@case.edu>
2025-04-04 11:48:50 -0400

    working on formatting for iphones and tablets

--- MatrixGame/script.js
+++ MatrixGame/script.js
@@ -3,7 +3,8 @@ let currentGameIndex = 0;
 let player1Data = [];
 let totalPoints = 0; // for tracking player 1's points across trials
 
-// const isMobile = window.mobileAndTabletCheck(); //use this as formatting condition
+const isMobile = mobileAndTabletCheck(); //use this as formatting condition
+console.log(isMobile);
 const filePath = './HeddenData.json';
 
 fetch(filePath)
@@ -29,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const instructionText = document.getElementById("instructionText");
 
 
-  instructionText.innerHTML = `<strong style="font-size: 20px;">Welcome to the Matrix Game!</strong><br>
+  instructionText.innerHTML = `<strong style="font-size: 20px;">Welcome to the Matrix Game!</strong><br><br>
   Your goal is to maximize the number of points you earn over the course of the following trials.<br><br>
   Your payoff at the end of each round will be the left number shown in the cell that the game ends on.<br>
   You will begin in cell A (as shown below) and can decide to Stay (end the game in the current cell) or Move (to the next cell).<br><br>
@@ -53,6 +54,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //// GAME SETUP
 async function setupGame() {
+  /// delete
+  console.log(isMobile);
+
   if (!gameData || currentGameIndex >= gameData.length) {
     document.getElementById("status").textContent = "All trials completed! Thanks for playing.";
     return;
@@ -221,7 +225,7 @@ async function setupGame() {
 
       player1Data.push({
         trial: game.Trial,
-        first_move: "move",
+        first_move: "stay",
         strategy: strategy,
         decisionTime: decisiontime,
         P1Payoff: game.P1Payoff,
@@ -269,9 +273,11 @@ function showNextTrialButton() {
     nextButton.remove();
     currentGameIndex++;
 
+    
     if (currentGameIndex < gameData.length) {
       setupGame();
     }
+    
     // last trial: create pop up for user to input instructions
     else {
       // Hide the game UI
@@ -316,35 +322,55 @@ function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function saveDataLocally() {
+// function saveDataLocally() {
+//   const dataStr = JSON.stringify(player1Data, null, 2); // Pretty-print JSON
+//   const blob = new Blob([dataStr], { type: "application/json" });
+//   const url = URL.createObjectURL(blob);
+//   const a = document.createElement("a");
+
+//   // Generate a unique filename using timestamp
+//   const timestamp = Date.now();
+//   const filename = `player1_data_${timestamp}.json`;
+
+
+//   a.href = url;
+//   a.download = filename;
+//   document.body.appendChild(a);
+//   a.click();
+//   document.body.removeChild(a);
+
+//   console.log("Local JSON file saved:", filename);
+// }
+
+
+
+async function sendToRedCap() {
+
+  ///////////// put below in helper and call it in setup game to create record
+  console.log("Saving data locally before sending to REDCap...");
+  const filename = `player_data.json`;
+
+  // moved save local
+  // saveDataLocally(); // Save locally first
   const dataStr = JSON.stringify(player1Data, null, 2); // Pretty-print JSON
   const blob = new Blob([dataStr], { type: "application/json" });
-  const url = URL.createObjectURL(blob);
+  const url_local = URL.createObjectURL(blob);
   const a = document.createElement("a");
-
-  // Generate a unique filename using timestamp
-  const timestamp = Date.now();
-  const filename = `player1_data_${timestamp}.json`;
-
-  a.href = url;
+  a.href = url_local;
   a.download = filename;
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
 
   console.log("Local JSON file saved:", filename);
-}
 
 
-
-function sendToRedCap() {
-  console.log("Saving data locally before sending to REDCap...");
-  saveDataLocally(); // Save locally first
+  /// Sending to Redcap below
 
   console.log("Preparing to send responses to REDCap");
   const url = 'https://redcap.case.edu/api/';
-  const timestamp = Date.now();
-  const filename = `player_data.json`;
+  const time = Date.now();
+  const timestamp = time.toString()
   const fileContent = JSON.stringify(player1Data);
 
   console.log("Filename:", filename);
@@ -378,6 +404,15 @@ function sendToRedCap() {
       console.error('Failed to create record to REDCap:', error);
     });
 
+
+
+      ///////////// put above in helper and call it in setup game to create record
+
+
+  // Pause for 0.5 seconds before proceeding
+  await delay(700);
+
+
   // OG
   formData.append('token', '2C941E2CCA757DF649E150366AD3904E');
   formData.append('content', 'file');
@@ -389,6 +424,7 @@ function sendToRedCap() {
   formData.append('file', file);
 
   console.log("Form Data:", formData);
+
 
   $.ajax({
     url: url,
@@ -405,9 +441,9 @@ function sendToRedCap() {
   });
 }
 
-// window.mobileAndTabletCheck = function() {
-//   let check = false;
-//   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-//   return check;
-// };
+async function mobileAndTabletCheck() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
 
